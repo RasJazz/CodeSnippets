12594e640d595fd6c31444f028a504a497b7ac21
RasJazz <jasmine.rasmussen.a@gmail.com>
2023-12-19 02:37:31 -0800

    Revert "Removing files for now"
    This reverts commit 9e01646789a1080ef189c32e4a70969d7c242dac.

--- AGL-FPS/EnemyBase.cs
+++ AGL-FPS/EnemyBase.cs
@@ -4,25 +4,35 @@ using UnityEngine.AI;
 public class EnemyBase : MonoBehaviour
 {
     protected NavMeshAgent enemyAINavMeshAgent; // variable for accessing NavMeshAgent AI features
-    [SerializeField] public Transform target; // player 
+    protected float distanceToTarget = Mathf.Infinity;
     
-    protected float distanceToTarget;
-    protected float chaseRange; // temp variable to hold initChaseRange, resets when player leashes enemy
+    [SerializeField] public Transform target; // player 
     [SerializeField] public float initialChaseRange;// Sets distance that enemy will follow player
+    protected float chaseRange; // temp variable to hold initChaseRange, resets when player leashes enemy
     [SerializeField] public float leashRange; // distance enemy will aggro player until player escapes
     [SerializeField] public float health;
+    protected string enemyName;
     
     protected EnemyBase()
     {
-        distanceToTarget = Mathf.Infinity;
-        chaseRange = initialChaseRange;
+        initialChaseRange = 15.0f;
+        chaseRange = 0.0f;
+        leashRange = 20.0f;
+        health = 0.0f;
+        enemyName = "noName";
+    }
+    
+    protected EnemyBase(float chaseRange, float leashRange, float health)
+    {
+        this.chaseRange = chaseRange;
+        this.leashRange = leashRange;
+        this.health = health;
     }
 
     protected virtual void EnemyAI()
     {
         enemyAINavMeshAgent = GetComponent<NavMeshAgent>();
     }
-        
     [HideInInspector] public void TakeDamage(float health)
     { 
         // decreases health by damage amount
