9e01646789a1080ef189c32e4a70969d7c242dac
RasJazz <jasmine.rasmussen.a@gmail.com>
2023-12-19 02:03:41 -0800

    Removing files for now

--- AGL-FPS/EnemyBase.cs
+++ AGL-FPS/EnemyBase.cs
@@ -4,35 +4,25 @@ using UnityEngine.AI;
 public class EnemyBase : MonoBehaviour
 {
     protected NavMeshAgent enemyAINavMeshAgent; // variable for accessing NavMeshAgent AI features
-    protected float distanceToTarget = Mathf.Infinity;
-    
     [SerializeField] public Transform target; // player 
-    [SerializeField] public float initialChaseRange;// Sets distance that enemy will follow player
+    
+    protected float distanceToTarget;
     protected float chaseRange; // temp variable to hold initChaseRange, resets when player leashes enemy
+    [SerializeField] public float initialChaseRange;// Sets distance that enemy will follow player
     [SerializeField] public float leashRange; // distance enemy will aggro player until player escapes
     [SerializeField] public float health;
-    protected string enemyName;
     
     protected EnemyBase()
     {
-        initialChaseRange = 15.0f;
-        chaseRange = 0.0f;
-        leashRange = 20.0f;
-        health = 0.0f;
-        enemyName = "noName";
-    }
-    
-    protected EnemyBase(float chaseRange, float leashRange, float health)
-    {
-        this.chaseRange = chaseRange;
-        this.leashRange = leashRange;
-        this.health = health;
+        distanceToTarget = Mathf.Infinity;
+        chaseRange = initialChaseRange;
     }
 
     protected virtual void EnemyAI()
     {
         enemyAINavMeshAgent = GetComponent<NavMeshAgent>();
     }
+        
     [HideInInspector] public void TakeDamage(float health)
     { 
         // decreases health by damage amount
